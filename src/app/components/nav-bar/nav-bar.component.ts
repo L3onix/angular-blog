20eac2f08257f25dc2b318c508735fb5d79d6f5a
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-01-19 13:57:09 -0300

    finalizando title e ajustando navbar

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMugHot, faFan } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
-
+  faMugHot = faMugHot;
+  faFan = faFan
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
+import { TitleComponent } from '../../components/title/title.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    NavBarComponent
+    NavBarComponent,
+    TitleComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
