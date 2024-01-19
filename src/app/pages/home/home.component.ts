710fcfbbc63a678bf4b20f56e0b8525d4edaf752
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-01-19 16:04:31 -0300

    estruturando posts recentes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { BigCardComponent } from '../../components/big-card/big-card.component';
 import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
+import { SmallCardComponent } from '../../components/small-card/small-card.component';
 import { TitleComponent } from '../../components/title/title.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
+    BigCardComponent,
     NavBarComponent,
-    TitleComponent
+    TitleComponent,
+    SmallCardComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
