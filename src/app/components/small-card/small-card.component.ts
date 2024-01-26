ef76b9cfcb07aeb46376e536ec533df6e47a68d2
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-01-25 22:43:37 -0300

    finalizando small-card

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { PostTagComponent } from '../post-tag/post-tag.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-small-card',
   standalone: true,
-  imports: [],
+  imports: [PostTagComponent, FontAwesomeModule],
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.css'
 })
 export class SmallCardComponent {
-
+  faCircle = faCircle
 }
