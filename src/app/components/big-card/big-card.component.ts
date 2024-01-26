bfc5803dde5394b9985b1fe4aa91d14bf57a070a
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-01-25 22:24:27 -0300

    finalizando big card

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { PostTagComponent } from '../post-tag/post-tag.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-big-card',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, PostTagComponent],
   templateUrl: './big-card.component.html',
   styleUrl: './big-card.component.css'
 })
 export class BigCardComponent {
-
+  faCircle = faCircle;
 }
