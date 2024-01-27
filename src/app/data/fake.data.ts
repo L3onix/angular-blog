8074fc097f7b49378cc8a4d5f4d92b0407b203dc
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-01-26 22:24:20 -0300

    criando estrutura de dados fake para carregamento dinâmico de páginas

---
+++ src/app/data/fake.data.ts
@@ -0,0 +1,31 @@
+export const data = {
+    posts: [
+        {
+            author: "",
+            date: "",
+            img: "",
+            summary: "",
+            tags: [
+                ""
+            ],
+            title: ""
+        }
+    ],
+    tags: [
+        {
+            name: "Design",
+            color: "#d50000",
+            backgroundColor: "#d5000088"
+        },
+        {
+            name: "Pesquisa",
+            color: "#6200ea",
+            backgroundColor: "#6200ea88"
+        },
+        {
+            name: "Apresentação",
+            color: "#00bfa5",
+            backgroundColor: "#00bfa588"
+        }
+    ]
+}
