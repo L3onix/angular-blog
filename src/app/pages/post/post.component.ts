e108a1f051f02452ad957d2c7d92fcd3b94c49c1
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-01-29 23:11:26 -0300

    finalizando design da página de post

--- src/app/pages/post/post.component.ts
+++ src/app/pages/post/post.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { PostTitleComponent } from '../../components/post-title/post-title.component';
+import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
+import { PostTagComponent } from '../../components/post-tag/post-tag.component';
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [PostTitleComponent],
+  imports: [NavBarComponent, PostTitleComponent, PostTagComponent],
   templateUrl: './post.component.html',
   styleUrl: './post.component.css'
 })
