3fac66ef4b87efbfa53257126888e895f027d56d
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-01-30 00:18:38 -0300

    carregamento dinâmico da home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,6 +3,7 @@ import { BigCardComponent } from '../../components/big-card/big-card.component';
 import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
 import { SmallCardComponent } from '../../components/small-card/small-card.component';
 import { TitleComponent } from '../../components/title/title.component';
+import { data } from '../../data/fake.data';
 
 @Component({
   selector: 'app-home',
@@ -17,5 +18,5 @@ import { TitleComponent } from '../../components/title/title.component';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  posts = data.posts
 }
